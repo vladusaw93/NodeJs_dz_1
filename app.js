131139c87d7eff3d42359281adc6172d2da89a40
vladusaw93 <59958848+vladusaw93@users.noreply.github.com>
2020-05-19 11:27:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-
 const fs = require('fs');
 const path = require('path');
 
-function copyFileFunction(pathFrom, pathTo, dir){
-
-    fs.readdirSync(`${dir}`).forEach(file => {
-        fs.rename(path.join(`${pathFrom}`, `${file}`),path.join(`${pathTo}`, `${file}`), err => {
-            console.log(err);
+function copyFileFunction(pathFrom, pathTo, dir) {
+    fs.readdir(path.join(`${dir}`), (err, files) => {
+        files.forEach(file => {
+            fs.rename(path.join(`${pathFrom}`, `${file}`), path.join(`${pathTo}`, `${file}`), err => {
+                console.log(err);
+            })
         })
     })
 }
 
-copyFileFunction(`./1800`, `./swap`,'./1800')
-copyFileFunction(`./2000`, `./1800`,'./2000')
-copyFileFunction(`./swap`, `./2000`,'./swap')
+
+const delay = () => {
+    return new Promise((resolve, reject) => setTimeout(resolve));
+}
+delay()
+    .then(() => {
+        copyFileFunction(`1800`, `./swap`, './1800');
+        return delay()
+    })
+    .then(() => {
+        copyFileFunction(`./2000`, `./1800`, './2000');
+        return delay()
+    })
+    .then(() => {
+        copyFileFunction(`./swap`, `./2000`, './swap');
+    })
+
